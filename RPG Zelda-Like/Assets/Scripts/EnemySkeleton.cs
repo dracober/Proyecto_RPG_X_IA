a918a749defdf5a56c1ee1f31c6d228c5e4d483b
fab526 <36773024+fab526@users.noreply.github.com>
2018-12-11 13:44:00 -0600

    Union_Proyecto_Gaona_Mejia
    New Features. Se creo un enemigo que ataca tipo melee, se creo un mapa en donde ira el nuevo boss, se creo un npc que interactua con el jugador
    Bugs. El enemigo en melee no le baja vida al player

---
+++ RPG Zelda-Like/Assets/Scripts/EnemySkeleton.cs
@@ -0,0 +1,259 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class EnemySkeleton : MonoBehaviour {
+
+    public float visionRadius;
+    public float attackRadius;
+    public float speed;
+
+    
+    ///--- Variables relacionadas con la vida
+    [Tooltip("Puntos de vida")]
+    public int maxHp = 3;
+    [Tooltip("Vida actual")]
+    public int hp;
+
+    public float attackSpeed = 2f;
+    bool attacking;
+
+    Vector2 mov;
+
+    CircleCollider2D attackCollider;
+
+    // Variable para guardar al jugador
+    GameObject player;
+
+    // Variable para guardar la posición inicial
+    Vector3 initialPosition, target;
+
+    // Animador y cuerpo cinemático con la rotación en Z congelada
+    Animator anim;
+    Rigidbody2D rb2d;
+
+    // Use this for initialization
+    void Start () {
+
+        // Recuperamos al jugador gracias al Tag
+        player = GameObject.FindGameObjectWithTag("Player");
+
+        // Guardamos nuestra posición inicial
+        initialPosition = transform.position;
+
+        anim = GetComponent<Animator>();
+        rb2d = GetComponent<Rigidbody2D>();
+
+        attackCollider = transform.GetChild(0).GetComponent<CircleCollider2D>();
+        attackCollider.enabled = false;
+
+        ///--- Iniciamos la vida
+        hp = maxHp;
+
+        
+
+    }
+	
+	// Update is called once per frame
+	void Update () {
+
+        slashattack();
+
+        
+
+        // Por defecto nuestro target siempre será nuestra posición inicial
+        target = initialPosition;
+
+        // Comprobamos un Raycast del enemigo hasta el jugador
+        RaycastHit2D hit = Physics2D.Raycast(
+            transform.position,
+            player.transform.position - transform.position,
+            visionRadius,
+            1 << LayerMask.NameToLayer("Default")
+        // Poner el propio Enemy en una layer distinta a Default para evitar el raycast
+        // También poner al objeto Attack y al Prefab Slash una Layer Attack 
+        // Sino los detectará como entorno y se mueve atrás al hacer ataques
+        );
+
+        // Aquí podemos debugear el Raycast
+        Vector3 forward = transform.TransformDirection(player.transform.position - transform.position);
+        Debug.DrawRay(transform.position, forward, Color.red);
+
+        // Si el Raycast encuentra al jugador lo ponemos de target
+        if (hit.collider != null)
+        {
+            if (hit.collider.tag == "Player")
+            {
+                target = player.transform.position;
+            }
+        }
+
+        // Calculamos la distancia y dirección actual hasta el target
+        float distance = Vector3.Distance(target, transform.position);
+        Vector3 dir = (target - transform.position).normalized;
+
+        // Si es el enemigo y está en rango de ataque nos paramos y le atacamos
+        if (target != initialPosition && distance < attackRadius)
+        {
+            // Aquí le atacaríamos, pero por ahora simplemente cambiamos la animación
+            anim.SetFloat("movX", dir.x);
+            anim.SetFloat("movY", dir.y);
+            //anim.Play("Skeleton_Chase_Right", -1, 0);  // Congela la animación de andar
+
+            ///-- Empezamos a atacar (importante una Layer en ataque para evitar Raycast)
+            if (!attacking) StartCoroutine(Attack(attackSpeed));
+
+
+        }
+        // En caso contrario nos movemos hacia él
+        else
+        {
+            rb2d.MovePosition(transform.position + dir * speed * Time.deltaTime);
+
+            // Al movernos establecemos la animación de movimiento
+            anim.speed = 1;
+            anim.SetFloat("movX", dir.x);
+            anim.SetFloat("movY", dir.y);
+            anim.SetBool("walking", true);
+        }
+
+        // Una última comprobación para evitar bugs forzando la posición inicial
+        if (target == initialPosition && distance < 0.05f)
+        {
+            transform.position = initialPosition;
+            // Y cambiamos la animación de nuevo a Idle
+            anim.SetBool("walking", false);
+        }
+
+        // Y un debug optativo con una línea hasta el target
+        Debug.DrawLine(transform.position, target, Color.green);
+    }
+
+    // Podemos dibujar el radio de visión y ataque sobre la escena dibujando una esfera
+    void OnDrawGizmosSelected()
+    {
+
+        Gizmos.color = Color.yellow;
+        Gizmos.DrawWireSphere(transform.position, visionRadius);
+        Gizmos.DrawWireSphere(transform.position, attackRadius);
+
+    }
+
+    IEnumerator Attack(float seconds)
+    {
+        if (mov != Vector2.zero)
+        {
+            attackCollider.offset = new Vector2(mov.x / 2, mov.y / 2);
+        }
+
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+        bool attacking = stateInfo.IsName("Skeleton_Slash");
+        attacking = true;  // Activamos la bandera
+
+
+        // Si tenemos objetivo y el prefab es correcto creamos la roca
+       /* if (target != initialPosition && !attacking)
+        {
+            anim.SetTrigger("attacking");
+            //Instantiate("Skeleton_Slash", transform.position, transform.rotation);
+            // Esperamos los segundos de turno antes de hacer otro ataque
+            yield return new WaitForSeconds(seconds);
+        }*/
+
+        if (attacking == false)
+        {
+            anim.SetBool("attacking", false);
+        }
+
+        if (attacking == true)
+        {
+            anim.SetBool("attacking", true);
+        }
+        attacking = false; // Desactivamos la bandera
+
+
+        if (attacking)
+        { // El normalized siempre resulta ser un ciclo entre 0 y 1 
+            float playbackTime = stateInfo.normalizedTime;
+
+            if (playbackTime > 0.33 && playbackTime < 0.66) attackCollider.enabled = true;
+            else attackCollider.enabled = false;
+        }
+
+        yield return new WaitForSeconds(seconds);
+        
+    }
+
+   
+
+    
+
+   void slashattack ()
+    {
+        // Vamos actualizando la posición de la colisión de ataque
+        if (mov != Vector2.zero)
+        {
+            attackCollider.offset = new Vector2(mov.x / 2, mov.y / 2);
+        }
+
+        // Buscamos el estado actual mirando la información del animador
+        AnimatorStateInfo stateInfo = anim.GetCurrentAnimatorStateInfo(0);
+        bool attacking = stateInfo.IsName("Skeleton_Slash");
+
+        // Detectamos el ataque, tiene prioridad por lo que va abajo del todo
+        /*if ((Input.GetKeyDown("space") || Input.GetKeyDown(KeyCode.X)) && !attacking)
+        {
+            anim.SetTrigger("attacking");
+        }*/
+
+        // Activamos el collider a la mitad de la animación de ataque
+        if (attacking)
+        { // El normalized siempre resulta ser un ciclo entre 0 y 1 
+            float playbackTime = stateInfo.normalizedTime;
+
+            if (playbackTime > 0.33 && playbackTime < 0.66) attackCollider.enabled = true;
+            else attackCollider.enabled = false;
+        }
+
+    }
+
+   /* void OnTriggerEnter2D(Collider2D col)
+    {
+        // Si chocamos contra el jugador o un ataque la borramos
+        //if (col.transform.tag == "Player" || col.transform.tag == "Attack"){
+       // Destroy(gameObject); 
+        //}
+        if (col.tag != "Player" && col.tag != "Attack")
+        {
+            ///--- Restamos uno de vida si es un enemigo
+            if (col.tag == "Enemy") col.SendMessage("Attacked");
+            Destroy(gameObject);
+        }
+    }*/
+
+    ///--- Gestión del ataque, restamos una vida
+    public void Attacked()
+    {
+        if (--hp <= 0) Destroy(gameObject);
+    }
+
+    
+
+    ///---  Dibujamos las vidas del enemigo en una barra 
+    void OnGUI()
+    {
+        // Guardamos la posición del enemigo en el mundo respecto a la cámara
+        Vector2 pos = Camera.main.WorldToScreenPoint(transform.position);
+
+        // Dibujamos el cuadrado debajo del enemigo con el texto
+        GUI.Box(
+            new Rect(
+                pos.x - 20,                   // posición x de la barra
+                Screen.height - pos.y + 60,   // posición y de la barra
+                40,                           // anchura de la barra    
+                24                            // altura de la barra  
+            ), hp + "/" + maxHp               // texto de la barra
+        );
+    }
+}
+
---
+++ RPG Zelda-Like/Assets/Scripts/NPC.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class NPC : MonoBehaviour {
+
+    [SerializeField] GameObject msgPanel;
+    
+
+	// Use this for initialization
+	void Start () {
+        GetComponent<Collider2D>().isTrigger = true;
+        msgPanel.SetActive(false);
+	}
+
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if(other.name == "Player")
+        {
+            print("Hit NPC");
+            msgPanel.SetActive(true);
+        }
+        
+    }
+
+    private void OnTriggerExit2D(Collider2D other)
+    {
+        if (other.name == "Player")
+        {
+            
+            msgPanel.SetActive(false);
+        }
+
+    }
+
+    // Update is called once per frame
+    void Update () {
+		
+	}
+}
